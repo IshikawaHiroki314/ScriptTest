e3b72d2e55f86fdca5e86eb6ee83650ef5431b14
IshikawaHiroki314 <102946554+IshikawaHiroki314@users.noreply.github.com>
2022-04-15 00:09:15 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int mp = 53;
+
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("���@�U���������B�c��MP��" + mp);
+        }
+        {
+            Debug.Log("MP������Ȃ����߁A���@���g���Ȃ��B");
+        }
+    }
+}
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -17,6 +33,12 @@ public class Test : MonoBehaviour
             Debug.Log(array[4 - i]);
         }
 
+        Boss lastboss = new Boss();
+        for (int i = 0; i < 10; i++)
+        {
+            lastboss.Magic();
+        }
+        lastboss.Magic();
     }
 
     // Update is called once per frame
